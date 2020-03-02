9a2e0ac539d1a9bbc71994f4feb75cc9b4719025
ninhxtina <christinaninh07@yahoo.com>
2020-03-01 20:42:36 -0800

    added hashmap basics

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
+/** By: Christina Ninh
+ * (https://www.daniweb.com/programming/software-development/threads/127733/help-to-design-a-simple-search-engine-in-java)
+ */
 public class MyMiniSearchEngine {
     // default solution. OK to change.
     // do not change the signature of index()
@@ -26,6 +30,24 @@ public class MyMiniSearchEngine {
     // return an empty list if search() finds no match in all documents.
     public List<Integer> search(String keyPhrase) {
         // homework
-        return new ArrayList<>(); // place holder
+        Hashtable<String, ArrayList<String>> ht = new Hashtable<String, ArrayList<String>>();
+        while (true) {
+            String[] result = keyPhrase.split("\\s"); //tokenizes the string into separate strings
+            //based on spaces only
+            for (int i = 0; i < result.length; i++) {
+                if (!ht.containsKey(result[i])) {
+                    ArrayList<String> temp = new ArrayList<String>(1);
+                    temp.add(keyPhrase);
+                    ht.put(result[i], temp);//assigns a key to anon arraylist that stores the value
+                } else {
+                    ArrayList<String> temp = (ArrayList<String>) ht.get(result[i]);
+                    temp.add(keyPhrase);
+                }
+            }
+            return new ArrayList<>(); //placeholder
+        }
     }
 }
+
+
+
