a168899712e5fdc3557be0f84def679819760760
ninhxtina <christinaninh07@yahoo.com>
2020-03-02 11:02:24 -0800

    added temp to testwordnotfound

--- src/MyMiniSearchEngineTest.java
+++ src/MyMiniSearchEngineTest.java
@@ -74,6 +74,17 @@ public class MyMiniSearchEngineTest {
     @Test
     public void testWordNotFound() {
         // homework
-        assertTrue(false); // place holder
+       /** MyMiniSearchEngine engine = new MyMiniSearchEngine(documents());
+
+        String[] inputs = {
+                "summer sun fun",
+                "Summer Sun Fun",
+        };
+
+        for(String input : inputs) {
+            List<Integer> result = engine.search(input);
+            assertEquals(List.of(-1, result.size()));
+            assertEquals(List.of(-1),result);
+        } */
     }
 }
