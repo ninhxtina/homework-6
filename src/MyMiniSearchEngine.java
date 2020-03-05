0e4191e8081e67043d3aa2cc02ae63be00899903
ninhxtina <christinaninh07@yahoo.com>
2020-03-04 22:13:41 -0800

    changed search

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -48,27 +48,18 @@ public class MyMiniSearchEngine {
     // return an empty list if search() finds no match in all documents.
     public List<Integer> search(String keyPhrase) {
         // homework
-        //if it can't find a match, return an empty list
-        if(keyPhrase == null) {
-            return new ArrayList<>();
-        }
+        String[] result = keyPhrase.split("\\s"); //tokenizes the line into separate strings based on spaces only
 
-        Hashtable<String, ArrayList<String>> ht = new Hashtable<String, ArrayList<String>>();
-        while (true) {
-            String[] result = keyPhrase.split("\\s"); //tokenizes the string into separate strings
-            //based on spaces only
-            for (int i = 0; i < result.length; i++) {
-                if (!ht.containsKey(result[i])) {
-                    ArrayList<String> temp = new ArrayList<String>(1);
-                    temp.add(keyPhrase);
-                    ht.put(result[i], temp);//assigns a key to anon arraylist that stores the value
-                } else {
-                    ArrayList<String> temp = (ArrayList<String>) ht.get(result[i]);
-                    temp.add(keyPhrase);
-                }
+        List<Integer> location = new ArrayList<>();
+
+        for(int i = 0; i < result.length; i++) {
+            if(!indexes.containsKey(keyPhrase)) {
+
+            } else {
+                return new ArrayList<>(); //return empty list if no matches
             }
-            return (List<Integer>) ht; //hashtable can't cast to list
         }
+        return location;
     }
 }
 
